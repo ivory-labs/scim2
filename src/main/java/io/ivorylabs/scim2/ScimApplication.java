27ffd843f977f373daf33a9712ca9023b4b2a182
Afrar Malakooth <mmafrar@gmail.com>
2024-11-03 22:12:34 +0800

    chore: Setting up the Spring project (#1)
    * chore: Setting up the Spring project
    
    * chore: Update project name and Gradle plugins

---
+++ src/main/java/io/ivorylabs/scim2/ScimApplication.java
@@ -0,0 +1,13 @@
+package io.ivorylabs.scim2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ivorylabs/scim2/ScimApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ivorylabs.scim2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
